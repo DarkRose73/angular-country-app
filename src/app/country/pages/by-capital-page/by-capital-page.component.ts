4e047283a378787cae3b74b0c46b57bed5e338c1
Joan Salas Silva <joan.salas.silva@gmail.com>
2025-07-28 22:38:01 -0400

    rutas y estructura de carpetas

---
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  imports: [],
+  templateUrl: './by-capital-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ByCapitalPageComponent {
+
+}
