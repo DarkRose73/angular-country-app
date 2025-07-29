f4a23f255f67130a59495f54bdb22200c8e25558
Joan Salas Silva <joan.salas.silva@gmail.com>
2025-07-28 22:45:18 -0400

    layout components

---
+++ src/app/country/layouts/country-layout/country-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './country-layout.component.html',
+})
+export class CountryLayoutComponent { }
