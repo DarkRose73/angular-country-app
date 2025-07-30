d0f7e4b7b7b873209ad711cbf61642a49c31dc03
Joan Salas Silva <joan.salas.silva@gmail.com>
2025-07-29 20:16:03 -0400

    menu superior e iconos

---
+++ src/app/country/components/top-menu/top-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'country-top-menu',
+  imports: [
+    RouterModule,
+  ],
+  templateUrl: './top-menu.component.html',
+})
+export class TopMenuComponent { }
--- src/app/country/layouts/country-layout/country-layout.component.ts
+++ src/app/country/layouts/country-layout/country-layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TopMenuComponent } from "../../components/top-menu/top-menu.component";
 
 @Component({
   selector: 'app-country-layout',
   imports: [
-    RouterModule
-  ],
+    RouterModule,
+    TopMenuComponent
+],
   templateUrl: './country-layout.component.html',
 })
 export class CountryLayoutComponent { }
