724b5f2b0d5e7509132ebc44ab3eabb3c6ce0fc7
Joan Salas Silva <joan.salas.silva@gmail.com>
2025-08-06 17:59:55 -0400

    countryService - Interfaces

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CountrySearchInputComponent } from "../../components/country-search-input/country-search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,9 +10,12 @@ import { CountryListComponent } from "../../components/country-list/country-list
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ByCapitalPageComponent {
+  private readonly countryService = inject(CountryService);
 
-  onSearch(val: string): void {
-    console.log(val);
+  onSearch(query: string): void {
+    this.countryService.searchByCaptial(query)
+      .subscribe((res) => {
+        console.log(res);
+      })
   }
-
 }
