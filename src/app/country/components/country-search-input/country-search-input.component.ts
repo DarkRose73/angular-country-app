0fb3789d32f19ec8e3a19f73a571ff77a4661b32
Joan Salas Silva <joan.salas.silva@gmail.com>
2026-02-04 22:49:13 -0300

    137. Debounce - busquedas automaticas

--- src/app/country/components/country-search-input/country-search-input.component.ts
+++ src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import {Component, effect, input, output, signal} from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -6,10 +6,25 @@ import { Component, input, output } from '@angular/core';
   templateUrl: './country-search-input.component.html',
 })
 export class CountrySearchInputComponent {
-  search = output<string>();
+  value = output<string>();
   placeholder = input<string>('Buscar');
 
+  inputValue = signal<string>('');
+
   onSearch(val: string): void {
-    this.search.emit(val);
+    this.value.emit(val);
   }
+
+  debounceEffect = effect((onCleanUp) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, 500);
+
+    onCleanUp(() => {
+      clearTimeout(timeout)
+    });
+  });
 }
+
